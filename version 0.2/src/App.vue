2e91bd61476c63c92851acf18bf8226163f6f35f
contact@bryanfreedman.com <contact@bryanfreedman.com>
2024-12-18 10:42:40 -0500

    dec 18 2024 10:42AM EST added starter code

---
+++ version 0.2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <site-header />
+    <router-view />
+    <site-footer />
+  </div>
+</template>
+
+<script>
+  import SiteHeader from './components/SiteFooter.vue';
+  import SiteFooter from './components/SiteHeader.vue';
+  export default {
+    components: {
+      SiteHeader,
+      SiteFooter
+    }
+  }
+</script>
+
+<style>
+</style>
