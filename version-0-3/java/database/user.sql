fae6b0f3f823abae1108bd777e1097fe67623541
contact@bryanfreedman.com <contact@bryanfreedman.com>
2024-12-19 16:27:42 -0500

    12/19/2024 4:27PM EST successful auth database. Can create new user and log in

--- version-0-3/java/database/user.sql
+++ version-0-3/java/database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
-WITH PASSWORD 'finalcapstone';
+CREATE USER closet_app_owner
+WITH PASSWORD 'closetappowner';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO closet_app_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO closet_app_owner;
 
-CREATE USER final_capstone_appuser
-WITH PASSWORD 'finalcapstone';
+CREATE USER closet_app_appuser
+WITH PASSWORD 'closetappuser';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_appuser;
+TO closet_app_appuser;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_appuser;
+TO closet_app_appuser;
